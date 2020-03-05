93d5233ad92a1180c11099ba2cc26b7228754a75
craig <craigahenry50@gmail.com>
2020-03-05 17:23:41 +0000

    Added logging macros

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -1,8 +1,7 @@
 #pragma once
 // For use by Hazel apps
-#include <stdio.h>
 #include "Hazel\Application.h"
-
+#include "Hazel\Log.h"
 // --------Entry Point ------
 #include "Hazel\EntryPoint.h"
 // --------Entry Point ------
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,14 +1,19 @@
 #pragma once
 
 #ifdef HZ_PLATFORM_WINDOWS
-
+ 
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hazel Engine:");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialised Log!");
+	int a = 6;
+	HZ_CLIENT_INFO("Initialised Log! {0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
 }
 #endif
+
